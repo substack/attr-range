1d5b79f88297c7601ed01d743fdad4c82f93be65
James Halliday <mail@substack.net>
2014-01-18 16:35:10 -0800

    take the start and end names as arguments

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-module.exports = function (cb) {
-    return function (elem, start, end) {
-        if (!end) {
-            var props = elem.properties || [];
-            for (var i = 0; i < props.length; i++) {
-                if (props[i].value === start) {
-                    var startKey = props[i].name;
-                    var endKey = startKey.replace(/(^|-)start$/, 'end');
-                    elem.getAttribute(endKey);
-                }
-            }
-        }
+module.exports = function (kstart, kend, cb) {
+    if (typeof start === 'object') {
+        cb = kend;
+        kend = kstart.end;
+        kstart = kstart.start;
+    }
+    if (typeof kstart === 'function') {
+        cb = kstart;
+        kstart = undefined;
+        kend = undefined;
+    }
+    if (typeof kend === 'function') {
+        cb = kend;
+        kend = undefined;
+    }
+    if (!kstart) kstart = 'data-start';
+    if (!kend) kend = 'data-end';
+    
+    return function (elem) {
+        var start = elem.getAttribute(kstart);
+        var end = elem.getAttribute(kend);
         
         cb({
-            start: start,
-            end: end,
+            start: start, end: end,
             range: { start: start, end: end },
             element: elem
         });
